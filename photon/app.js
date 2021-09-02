6e56775aa3840d58391595fa90a234b030d849cd
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-09-02 18:15:22 -0300

    generate pictures all set

--- photon/app.js
+++ photon/app.js
@@ -6,6 +6,8 @@ const searchInput = document.querySelector('.search-input');
 let searchValue;
 let currentSearch;
 const more = document.querySelector('.more');
+let page = 1;
+let fetchLink;
 
 //event listeners
 searchInput.addEventListener('input', updateInput);
@@ -31,3 +33,33 @@ async function fetchApi(url){
     const data = await dataFetch.json();
     return data;
 }
+function generatePictures(data){
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `
+        <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a target="_blank" href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}</img>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+async function curatedPhotos(){
+    fetchLink = 'https://api.pexels.com/v1/curated?per_page=16&page=1';
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+async function searchPhotos(query){
+    clear();
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+function clear(){
+    gallery.innerHTML = '';
+    searchInput.value = '';
+}
+curatedPhotos();
