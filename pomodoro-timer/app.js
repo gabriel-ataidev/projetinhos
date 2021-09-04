e940ec31396ac24f427af79293dddc453ac55a15
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-09-04 12:16:01 -0300

    pomodoro timer all set

--- pomodoro-timer/app.js
+++ pomodoro-timer/app.js
@@ -1,4 +1,37 @@
 //selectors
 const minutesEl = document.querySelector("#minutes");
 const secondsEl = document.querySelector("#seconds");
+let minutes, seconds, counterInterval = undefined;
 
+//functions
+resetCounter();
+function updateCounterEl(){
+    minutesEl.innerHTML  = minutes < 10 ? `0${minutes}` : minutes;
+    secondsEl.innerHTML  = seconds < 10 ? `0${seconds}` : seconds;
+}
+function startCounter(){
+    if(counterInterval) return;
+    counterInterval = setInterval( () => {
+        if(seconds === 0){
+            if(minutes === 0) return destroyInterval();
+            seconds = 59;
+            --minutes;
+        }else{
+            --seconds
+        }
+        updateCounterEl();
+    }, 1000)
+}
+function pauseCounter(){
+    destroyInterval();   
+}
+function destroyInterval(){
+    clearInterval(counterInterval);
+    counterInterval;
+}
+function resetCounter(){
+    destroyInterval();
+    minutes = 25;
+    seconds = 00;
+    updateCounterEl();
+}
