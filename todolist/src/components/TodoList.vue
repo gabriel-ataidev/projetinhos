de3e72292e47d682793b12fdce4714d2419e2a06
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-10 00:10:20 -0300

    li v-for all set

--- todolist/src/components/TodoList.vue
+++ todolist/src/components/TodoList.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="todo-list">
-    hello
+    <h3>Todo List</h3>
+    <input type="text">
+
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <input type="checkbox" v-model="item.checked">
+        {{ item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TodoList',
-  props: {
-    
+  data() {
+    return {
+      list: [ {label: 'fazer almoço', checked: true},
+      {label: 'fazer almoço 2', checked: false} ]
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  ul{
+    list-style: none;
+    padding: 0;
+  }
 </style>
