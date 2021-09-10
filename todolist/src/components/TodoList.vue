5ee1c249d6c53d22d3dec70aa1622ed567c02d3c
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-09 23:57:32 -0300

    cleaning the code

---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="todo-list">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
