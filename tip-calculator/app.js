21729dc6a55a0fe150d29dc19c09da0e37ca423e
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-29 21:13:25 -0300

    getting the correct percentage values

--- tip-calculator/app.js
+++ tip-calculator/app.js
@@ -1,61 +1,59 @@
+// SELECTORS
+const billInput = document.querySelector(".bill-input");
+const customInput = document.querySelector(".custom");
+const peopleInput = document.querySelector(".people-input");
+const percentageButton5 = document.querySelector(".n5");
+const percentageButton10 = document.querySelector(".n10");
+const percentageButton15 = document.querySelector(".n15");
+const percentageButton25 = document.querySelector(".n25");
+const percentageButton50 = document.querySelector(".n50");
+var tipValue;
+var billValue;
 
+// EVENT LISTENERS
+billInput.addEventListener("change", submitValue);
+customInput.addEventListener("change", submitCustom);
+peopleInput.addEventListener("change", submitPeople);
+percentageButton5.addEventListener("click", percentageValue5);
+percentageButton10.addEventListener("click", percentageValue10);
+percentageButton15.addEventListener("click", percentageValue15);
+percentageButton25.addEventListener("click", percentageValue25);
+percentageButton50.addEventListener("click", percentageValue50);
 
 // FUNCTIONS
-function allFunctions(){
-    
-    // SELECTORS
-    const billInput = document.querySelector('.bill-input');
-    const customInput = document.querySelector('.custom');
-    const peopleInput = document.querySelector('.people-input');
-    const percentageButton5 = document.querySelector('.n5');
-    const percentageButton10 = document.querySelector('.n10');
-    const percentageButton15 = document.querySelector('.n15');
-    const percentageButton25 = document.querySelector('.n25');
-    const percentageButton50 = document.querySelector('.n50');
-    let tipValue;
-    
-    // EVENT LISTENERS
-    billInput.addEventListener('change', submitValue);
-    customInput.addEventListener('change', submitCustom);
-    peopleInput.addEventListener('change', submitPeople);
-    percentageButton5.addEventListener('click', percentageValue5);
-    percentageButton10.addEventListener('click', percentageValue10);
-    percentageButton15.addEventListener('click', percentageValue15);
-    percentageButton25.addEventListener('click', percentageValue25);
-    percentageButton50.addEventListener('click', percentageValue50);
-
-    
-    function submitValue() {
-        const billValue = billInput.value;
-        console.log(billValue);
-    }
-    function submitCustom() {
-        tipValue = customInput.value * 0.01;
-        console.log(tipValue);
-    }
-    function submitPeople() {
-        const peopleValue = peopleInput.value;
-        console.log(peopleValue);
-    }
-    function percentageValue5() {
-        tipValue = 0.05;
-        console.log(tipValue);
-    }
-    function percentageValue10() {
-        tipValue = 0.1;
-        console.log(tipValue);
-    }
-    function percentageValue15() {
-        tipValue = 0.15;
-        console.log(tipValue);
-    }
-    function percentageValue25() {
-        tipValue = 0.25;
-        console.log(tipValue);
-    }
-    function percentageValue50() {
-        tipValue = 0.50;
-        console.log(tipValue);
-    }
+function submitValue() {
+  billValue = billInput.value;
 }
-allFunctions();
+function submitCustom() {
+  tipValue = customInput.value * 0.01 + 1;
+}
+function submitPeople() {
+  let peopleValue = peopleInput.value;
+  //tip-amout
+  var tipAmount = (billValue * (tipValue - 1)) / peopleValue;
+  console.log(tipAmount);
+  //total
+  var totalAmount = (billValue * tipValue) / peopleValue;
+  //the change of values
+  
+}
+function percentageValue5() {
+  tipValue = 1 + 0.05;
+  customInput.value = 5;
+}
+function percentageValue10() {
+  tipValue = 1 + 0.1;
+  customInput.value = 10;
+}
+function percentageValue15() {
+  tipValue = 1 + 0.15;
+  customInput.value = 15;
+}
+function percentageValue25() {
+  tipValue = 1 + 0.25;
+  customInput.value = 25;
+}
+function percentageValue50() {
+  tipValue = 1 + 0.5;
+  customInput.value = 50;
+}
