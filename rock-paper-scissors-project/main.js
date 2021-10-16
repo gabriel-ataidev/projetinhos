298d797d3071d5f414ca6166cf036cfe5c9c5e8c
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-16 17:15:57 -0300

    rules modal

---
+++ rock-paper-scissors-project/main.js
@@ -0,0 +1,11 @@
+const rulesBg = document.querySelector('.rules-bg');
+
+
+
+
+function openRules() {
+    rulesBg.style.display = 'flex';
+}
+function closeRules() {
+    rulesBg.style.display = 'none';
+}
