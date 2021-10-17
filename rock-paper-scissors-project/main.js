1329b7a84b0973706f400943e7e6e7e42b0dae7f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-16 22:19:23 -0300

    selecting card

--- rock-paper-scissors-project/main.js
+++ rock-paper-scissors-project/main.js
@@ -6,7 +6,39 @@ function closeRules() {
   rulesBg.style.display = "none";
 }
 
+const main = document.querySelector("main");
+const scoreNumber = document.querySelector(".score-number");
 
+function rockSelected() {
+  main.style.display = "none";
+  result.style.display = "flex";
+  const rock = document.createElement("IMG");
+  rock.setAttribute("src", "images/icon-rock.svg");
+  chosenCard.classList.add("a-rock");
+  chosenCard.appendChild(rock);
+  value1 = 3;
+  secondCard();
+}
+function paperSelected() {
+  main.style.display = "none";
+  result.style.display = "flex";
+  const paper = document.createElement("IMG");
+  paper.setAttribute("src", "images/icon-paper.svg");
+  chosenCard.classList.add("a-paper");
+  chosenCard.appendChild(paper);
+  value1 = 2;
+  secondCard();
+}
+function scissorsSelected() {
+  main.style.display = "none";
+  result.style.display = "flex";
+  const scissors = document.createElement("IMG");
+  scissors.setAttribute("src", "images/icon-scissors.svg");
+  chosenCard.classList.add("a-scissors");
+  chosenCard.appendChild(scissors);
+  value1 = 1;
+  secondCard();
+}
 
 
 function openRules() {
