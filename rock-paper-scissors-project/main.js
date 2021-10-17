81e43431ad82bdc405706302dc5a1e0f4e90f78f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-16 22:19:03 -0300

    the rules button

--- rock-paper-scissors-project/main.js
+++ rock-paper-scissors-project/main.js
@@ -1,4 +1,10 @@
-const rulesBg = document.querySelector('.rules-bg');
+const rulesBg = document.querySelector(".rules-bg");
+function openRules() {
+  rulesBg.style.display = "flex";
+}
+function closeRules() {
+  rulesBg.style.display = "none";
+}
 
 
 
