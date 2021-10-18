d3e29474b7465c8e61b370088ef6df2160f9bb2f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-18 13:02:13 -0300

    showing and hiding cards

--- rock-paper-scissors-project/main.js
+++ rock-paper-scissors-project/main.js
@@ -12,35 +12,38 @@ const result = document.querySelector(".result");
 const chosenCard = document.querySelector(".chosen-card");
 const card2 = document.querySelector(".card2");
 const outcome = document.querySelector(".outcome");
+const playBtn = document.querySelector(".play-button");
+const win = document.querySelector(".win");
+const lose = document.querySelector(".lose");
+const draw = document.querySelector(".draw");
+const iconScissors = document.querySelector(".icon-scissors");
+const iconScissors2 = document.querySelector(".icon-scissors2");
+const iconPaper = document.querySelector(".icon-paper");
+const iconPaper2 = document.querySelector(".icon-paper2");
+const iconRock = document.querySelector(".icon-rock");
+const iconRock2 = document.querySelector(".icon-rock2");
+const bgCard = document.querySelector(".bg-card");
+
 let value1;
 
 function rockSelected() {
   main.style.display = "none";
   result.style.display = "flex";
-  const rock = document.createElement("IMG");
-  rock.setAttribute("src", "images/icon-rock.svg");
-  chosenCard.classList.add("a-rock");
-  chosenCard.appendChild(rock);
+  iconRock.style.display = 'flex';
   value1 = 3;
   secondCard();
 }
 function paperSelected() {
   main.style.display = "none";
   result.style.display = "flex";
-  const paper = document.createElement("IMG");
-  paper.setAttribute("src", "images/icon-paper.svg");
-  chosenCard.classList.add("a-paper");
-  chosenCard.appendChild(paper);
+  iconPaper.style.display = 'flex';
   value1 = 2;
   secondCard();
 }
 function scissorsSelected() {
   main.style.display = "none";
   result.style.display = "flex";
-  const scissors = document.createElement("IMG");
-  scissors.setAttribute("src", "images/icon-scissors.svg");
-  chosenCard.classList.add("a-scissors");
-  chosenCard.appendChild(scissors);
+  iconScissors.style.display = 'flex';
   value1 = 1;
   secondCard();
 }
@@ -49,37 +52,27 @@ function secondCard() {
   let value2 = Math.floor(Math.random() * 3) + 1;
   console.log(value2);
   setTimeout(() => {
+    playBtn.style.display = 'flex';
+    bgCard.style.display = 'none';
     if (value2 == 1) {
-      const scissors = document.createElement("IMG");
-      scissors.setAttribute("src", "images/icon-scissors.svg");
-      card2.classList.add("a-scissors");
-      card2.appendChild(scissors);
+      iconScissors2.style.display = 'flex';
     } else if (value2 == 2) {
-      const paper = document.createElement("IMG");
-      paper.setAttribute("src", "images/icon-paper.svg");
-      card2.classList.add("a-paper");
-      card2.appendChild(paper);
+      iconPaper2.style.display = 'flex';
     } else {
-      const rock = document.createElement("IMG");
-      rock.setAttribute("src", "images/icon-rock.svg");
-      card2.classList.add("a-rock");
-      card2.appendChild(rock);
+      iconRock2.style.display = 'flex';
     }
   }, 1000);
   setTimeout(() => {
     if (value1 == value2) {
-      const h1 = document.createElement("h1");
-      h1.innerText = "DRAW";
-      outcome.appendChild(h1);
-    } else if (value1 < value2 || value1 - value2 == 2) {
-      const h1 = document.createElement("h1");
-      h1.innerText = "YOU WIN";
-      outcome.appendChild(h1);
+      draw.style.display = 'flex';
+    } else if (value1 < value2 && value2 - value1 !== 2) {
+      win.style.display = 'flex';
+      ++scoreNumber.innerText;
+    } else if(value1 - value2 === 2) {
+      win.style.display = 'flex';
       ++scoreNumber.innerText;
     } else {
-      const h1 = document.createElement("h1");
-      h1.innerText = "YOU LOSE";
-      outcome.appendChild(h1);
+      lose.style.display = 'flex';
     }
   }, 1000);
 }
@@ -87,4 +80,15 @@ function secondCard() {
 function playButton() {
   main.style.display = "flex";
   result.style.display = "none";
+  playBtn.style.display = 'none';
+  draw.style.display = 'none';
+  win.style.display = 'none';
+  lose.style.display = 'none';
+  iconScissors.style.display = 'none';
+  iconRock.style.display = 'none';
+  iconPaper.style.display = 'none';
+  bgCard.style.display = 'flex';
+  iconRock2.style.display = 'none';
+  iconPaper2.style.display = 'none';
+  iconScissors2.style.display = 'none';
 }
