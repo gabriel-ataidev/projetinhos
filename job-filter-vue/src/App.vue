fdabb10c08ea0e073bd04f5d4e6ede72e1921995
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-23 19:03:58 -0300

    setting up filter container component

--- job-filter-vue/src/App.vue
+++ job-filter-vue/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FilterContainer msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FilterContainer from './components/FilterContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FilterContainer,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ job-filter-vue/src/components/FilterContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="filter-container">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FilterContainer',
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
