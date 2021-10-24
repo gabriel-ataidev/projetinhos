e74cdbed10df8a06d5f202b1cf6a8f5bfd7f35a1
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-23 22:04:54 -0300

    setting up json

--- job-filter-vue/src/components/FilterContainer.vue
+++ job-filter-vue/src/components/FilterContainer.vue
@@ -7,7 +7,28 @@
 <script>
 export default {
     name: "FilterContainer",
-
+    data() {
+        return{
+            // data: null,
+            // company: null,
+            // companies: null,
+        }
+    },
+    methods: {
+        async getData() {
+            const req = await fetch("http://localhost:3000/data");
+            const dataJson = await req.json();
+            console.log(dataJson);
+            // for (let i = 0; i < dataJson.length; i++) {
+            //     this.data = dataJson[i];
+            //     this.companies = dataJson[i].company;
+            //     console.log(dataJson[i].company);
+            // }
+        }
+    },
+    mounted() {
+        this.getData();
+    }
 }
 </script>
 
