5bed09b8fe6109cd5b4b8a046ebda3b1aa3c2af4
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-27 19:44:32 -0300

    fetching data from json

--- job-filter-vue/src/components/Cards.vue
+++ job-filter-vue/src/components/Cards.vue
@@ -5,9 +5,23 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: 'Cards',
 }
+  name: "Cards",
+  data() {
+    return {
+      cards: null,
+    };
+  },
+  created() {
+    axios.get("http://localhost:3000/data").then((response) => {
+      this.cards = response.data;
+    });
+  },
+};
 </script>
 
 <style>
