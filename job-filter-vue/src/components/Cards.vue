67eae11038acf6bfbfcf5acb45454af78866ff1d
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-27 20:38:18 -0300

    media query

--- job-filter-vue/src/components/Cards.vue
+++ job-filter-vue/src/components/Cards.vue
@@ -64,4 +64,147 @@ export default {
 
 <style>
 
+</style>#cards {
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+  align-items: center;
+  .card {
+    background: #fff;
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    width: 100%;
+    max-width: 80vw;
+    padding: 20px 30px;
+    border-radius: 5px;
+    filter: drop-shadow(0px 1px 10px #6ec4c43d);
+    .text {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      margin: 5px 20px;
+      min-height: 88px;
+      .line1 {
+        color: #5ba4a4;
+        font-size: 13px;
+        span + span {
+          margin-left: 6px;
+        }
+        .color-card {
+          text-align: center;
+          font-size: 11px;
+          color: white;
+          background: #5ba4a4;
+          padding: 7px 7px 5px;
+          border-radius: 14px;
+        }
+        .featured {
+          background: #000;
+        }
+      }
+      .line2 {
+        display: flex;
+        font-size: 12px;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        &:hover {
+          color: #5ba4a4;
+        }
+      }
+      .line3 {
+        color: #a2a3a3;
+        font-weight: 500;
+        font-size: 14px;
+        span {
+          padding-right: 15px;
+          & + span {
+            &::before {
+              content: "•";
+              padding-right: 15px;
+            }
+          }
+        }
+      }
+    }
+    .technologies {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      span {
+        font-size: 12px;
+        border: none;
+        padding: 9px 8px 6px;
+        margin: 0 8px;
+        background: #eff6f7;
+        color: #5ba4a4;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        &:hover {
+          color: #eff6f7;
+          background: #5ba4a4;
+        }
+      }
+    }
+  }
+  .card-disabled {
+    display: none;
+  }
+  .card-featured {
+    border-left: 5px solid #5ba4a4;
+  }
+}
+@media (max-width: 376px) {
+  #cards {
+    margin: 40px 25px;
+    .card {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 10px 15px;
+      margin: 0;
+      margin-top: 40px;
+      height: 100%;
+      min-height: fit-content;
+      position: relative;
+      .image {
+        position: absolute;
+        top: -30px;
+        left: 15px;
+        img {
+          max-width: 50px;
+          height: auto;
+        }
+      }
+      .text {
+        display: flex;
+        margin: 0;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-top: 30px;
+        .line1 {
+          font-size: 16px;
+        }
+        .line2 {
+          padding: 20px 0;
+        }
+        .line3 {
+          font-size: 13px;
+        }
+      }
+      .technologies {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: flex-start;
+        margin: 20px 0;
+        border-top: 1px solid #a2a3a3;
+        span {
+          margin: 16px 16px 0 0;
+        }
+      }
+    }
+  }
+}
 </style>
