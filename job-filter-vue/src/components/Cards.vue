afe5fb73d11196284eff2d0d31bdf3f73019d6c9
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-27 20:35:40 -0300

    creating cards

--- job-filter-vue/src/components/Cards.vue
+++ job-filter-vue/src/components/Cards.vue
@@ -1,6 +1,44 @@
 <template>
   <div id="cards">
     <p>cards is working</p>
+    <div
+      v-for="(n, index) in 10"
+      :key="index"
+      class="card"
+      :class="cardFeatured"
+    >
+      <div class="image"><img :src="cards[index].logo" /></div>
+      <div class="text">
+        <div class="line1">
+          <span>{{ cards[index].company }}</span
+          ><span v-show="cards[index].new" class="color-card new">NEW!</span
+          ><span
+            class="color-card"
+            :class="featured"
+            v-show="cards[index].featured"
+            >FEATURED</span
+          >
+        </div>
+        <div class="line2">
+          <h1>{{ cards[index].position }}</h1>
+        </div>
+        <div class="line3">
+          <span>{{ cards[index].postedAt }}</span
+          ><span>{{ cards[index].contract }}</span
+          ><span>{{ cards[index].location }}</span>
+        </div>
+      </div>
+      <div class="technologies">
+        <span>{{ cards[index].role }}</span>
+        <span>{{ cards[index].level }}</span>
+        <span v-for="(n, v) in 3" :key="v" v-show="cards[index].tools[v]">{{
+          cards[index].tools[v]
+        }}</span>
+        <span v-for="(n, i) in 3" :key="i" v-show="cards[index].languages[i]">{{
+          cards[index].languages[i]
+        }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
