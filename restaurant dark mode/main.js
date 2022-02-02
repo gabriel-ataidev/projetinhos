257bc4407ce270e1b0ad6a266f0cd9f7db2d99be
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-02-02 17:36:10 -0300

    hiding menu on nav-link click

--- restaurant dark mode/main.js
+++ restaurant dark mode/main.js
@@ -11,3 +11,12 @@ const showMenu = (toggleId, navId) => {
 };
 showMenu("nav-toggle", "nav-menu");
 
+// remove menu mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
