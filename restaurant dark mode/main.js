a4ddb8db40d2f1474d58553d655e8e0f175cd005
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-02-02 18:06:11 -0300

    some changes

--- restaurant dark mode/main.js
+++ restaurant dark mode/main.js
@@ -20,3 +20,27 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+// // scroll sections active link
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     sectionId = current.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.add("active-link");
+//     } else {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
+
