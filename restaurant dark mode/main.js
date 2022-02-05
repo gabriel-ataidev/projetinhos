cc6c8f1cd24d095f2b0189630c1c11ea8cd7e9ae
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-02-05 17:42:22 -0300

    saving chosen theme to local storage

--- restaurant dark mode/main.js
+++ restaurant dark mode/main.js
@@ -71,6 +71,15 @@ const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
 const iconTheme = 'bx-sun';
 themeButton.addEventListener('click', () => {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+if(selectedTheme){
+document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
