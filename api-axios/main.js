be646df5d508dafc27bb29ba8c3f1637dd972beb
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-02-21 18:04:34 -0300

    sending data to api

--- api-axios/main.js
+++ api-axios/main.js
@@ -1,11 +1,25 @@
 const url = "http://localhost:5500/api";
+const newUser = {
+  name: "Oliver",
+  avatar: "http://lorempixel.com.br/500/400/",
+  city: "São José",
+};
 
 function getUser() {
-    axios.get(url)
-    .then(response => {
-        const data = response.data;
-        renderResults.textContent = JSON.stringify(data);
+  axios
+    .get(url)
+    .then((response) => {
+      const data = response.data;
+      renderResults.textContent = JSON.stringify(data);
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 }
 getUser();
+
+function addNewUser() {
+  axios
+    .post(url, newUser)
+    .then((response) => alert(JSON.stringify(response.data)))
+    .catch((error) => console.log(error));
+}
+addNewUser();
